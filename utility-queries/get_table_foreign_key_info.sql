8cbe270eed2be9d8ee89f672cb58c21ef5546e50
José Ferreira <jose97df@gmail.com>
2026-02-05 10:23:27 +0000

    add
    add

--- utility-queries/get_table_foreign_key_info.sql
+++ utility-queries/get_table_foreign_key_info.sql
@@ -1,18 +1,20 @@
 -- Utility query to get foreign key information for a specified table
 -- Usage: Replace 'YourSearchTermHere' with your table name
 
-DECLARE @TableName NVARCHAR(128) = 'YourSearchTermHere';
+DECLARE @TableName NVARCHAR(256) ='YourSearchTermHere';
 
 SELECT 
-    fk.name AS ForeignKeyName,
-    OBJECT_NAME(fk.parent_object_id) AS TableName,
-    COL_NAME(fkc.parent_object_id, fkc.parent_column_id) AS ColumnName,
-    OBJECT_NAME (fk.referenced_object_id) AS ReferencedTableName,
-    COL_NAME(fkc.referenced_object_id, fkc.referenced_column_id) AS ReferencedColumnName
+    fk.name AS foreign_key_name,
+    OBJECT_NAME(fk.parent_object_id) AS referencing_table,
+    COL_NAME(fkc.parent_object_id, fkc.parent_column_id) AS referencing_column,
+    OBJECT_NAME(fk.referenced_object_id) AS referenced_table,
+    COL_NAME(fkc.referenced_object_id, fkc.referenced_column_id) AS referenced_column
 FROM 
     sys.foreign_keys AS fk
 INNER JOIN 
     sys.foreign_key_columns AS fkc 
     ON fk.object_id = fkc.constraint_object_id
 WHERE 
-    OBJECT_NAME(fk.parent_object_id) = @TableName
+    OBJECT_NAME(fk.referenced_object_id) = @TableName
+ORDER BY 
+    referencing_table;
